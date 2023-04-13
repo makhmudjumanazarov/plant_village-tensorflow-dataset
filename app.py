39ae89070aca10d5c47b1ce9335b8bf3a64b7fee
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-04-13 13:26:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ if st.button('Predict'):
         st.write(img_array.shape)
         img_array = np.expand_dims(img_array, axis=1)
         st.write(img_array.shape)
-#         img_array = img_array.transpose((1,0,2,3))  
+        img_array = img_array.transpose((1,0,2,3))
+        st.write(img_array.shape)
 #         val = model_load.predict(img_array)
 #         output_text = labels[np.argmax(val[0])]
 #         font_size = "24px"
